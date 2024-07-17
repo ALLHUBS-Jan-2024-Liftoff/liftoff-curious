e95b5f825a8003c42b828ef4664811e3690d8f7a
Niladri Sekhar Jyoti <nil@Niladris-MacBook-Pro.local>
2024-07-16 21:46:27 -0400

    Starter code added

---
+++ quiz-api/src/main/java/com/codersquiz/quiz_api/QuizApiApplication.java
@@ -0,0 +1,34 @@
+package com.codersquiz.quiz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class QuizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApiApplication.class, args);
+	}
+
+	// this is to tackle with the CORS error
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+
+
+}
---
+++ quiz-ui/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Starter } from './components/Starter';
+
+function App() {
+  return (
+    <div className="App">
+      <Starter/>
+    </div>
+  );
+}
+
+
+export default App;
