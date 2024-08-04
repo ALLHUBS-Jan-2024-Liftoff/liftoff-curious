0f95c412d1e91f0cba1528fe0b33a2daa883987b
molalenim <molaleni.mabaso@gmail.com>
2024-08-03 22:33:29 -0400

    basic contact us page structure

--- quiz-ui/src/pages/ContactPage.jsx
+++ quiz-ui/src/pages/ContactPage.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import JumbotronComponent from '../components/JumbotronComponent'
+import React from "react";
+import JumbotronComponent from "../components/JumbotronComponent";
 
 function ContactPage() {
   return (
     <>
-    <JumbotronComponent backgroundImage={'https://placehold.co/1600x400/008F67/008F67/png'} pageName={"Contact"}/>
-    <div style={{ minHeight: '600px', backgroundColor: 'antiquewhite'}} />
+      <JumbotronComponent
+        backgroundImage={"https://placehold.co/1600x400/008F67/008F67/png"}
+        pageName={"Contact"}
+      />
+      <div style={{ minHeight: "600px", backgroundColor: "antiquewhite" }}>
+      {/* Main container for contact page content */}
+      <div className="row">
+        <div className="col rounded pb-3">
+          <h4>Contact Us</h4>
+          <p>
+            Get in touch with us
+          </p>
+        </div>
+      </div>
+      {/* Grid container for Contact */}
+      <div className="row">
+        <div className="col col-12 col-lg-6 rounded order-last order-lg-first">
+          <h4>Have some feedback/Comments?</h4>
+          <div className="team-member d-flex align-items-center mb-3">
+            
+            <div className="ps-3">
+      Form
+              
+            </div>
+          </div>
+        </div>
+
+        {/* Grid container for How to Play*/}
+        <div className="col col-12 col-lg-6 rounded">
+          <h4>Comment by - some name</h4>
+          
+        </div>
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default ContactPage
+export default ContactPage;
+
+
