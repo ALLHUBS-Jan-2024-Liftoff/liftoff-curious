49b82056b8fc4a6dc0910d023f7efab9d9a6dfff
Niladri Sekhar Jyoti <niladri123@gmail.com>
2024-08-07 15:25:37 -0400

    Removed error of logout on refresh

--- quiz-ui/src/context/AuthContext.js
+++ quiz-ui/src/context/AuthContext.js
@@ -4,40 +4,42 @@ import axiosAuthInstance from '../services/axiosConfig';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [username, setUsername] = useState('');
+ const [authenticated, setAuthenticated] = useState(() => {
+   const storedAuth = localStorage.getItem('authenticated');
+   return storedAuth ? JSON.parse(storedAuth) : false;
+ });
+ const [username, setUsername] = useState(() => {
+   return localStorage.getItem('username') || '';
+ });
 
-  useEffect(() => {
-    // Load authentication state from local storage on app startup
-    const storedUsername = localStorage.getItem('username');
-    if (storedUsername) {
-      setAuthenticated(true);
-      setUsername(storedUsername);
-    }
-  }, []);
+ useEffect(() => {
+   // Save authentication state to local storage
+   localStorage.setItem('authenticated', JSON.stringify(authenticated));
+   localStorage.setItem('username', username);
+ }, [authenticated, username]);
 
-  const login = (username) => {
-    setAuthenticated(true);
-    setUsername(username);
-    localStorage.setItem('username', username);
-  };
+ const login = (username) => {
+   setAuthenticated(true);
+   setUsername(username);
+ };
 
-  const logout = async () => {
-    try {
-      await axiosAuthInstance.post('/users/logout');
-      setAuthenticated(false);
-      setUsername('');
-      localStorage.removeItem('username');
-    } catch (error) {
-      console.error('Logout failed', error);
-    }
-  };
+ const logout = async () => {
+   try {
+     await axiosAuthInstance.post('/users/logout');
+     setAuthenticated(false);
+     setUsername('');
+     localStorage.removeItem('authenticated');
+     localStorage.removeItem('username');
+   } catch (error) {
+     console.error('Logout failed', error);
+   }
+ };
 
-  return (
-    <AuthContext.Provider value={{ authenticated, username, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+ return (
+   <AuthContext.Provider value={{ authenticated, username, login, logout }}>
+     {children}
+   </AuthContext.Provider>
+ );
 };
 
 export default AuthContext;
